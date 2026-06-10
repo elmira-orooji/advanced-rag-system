890810d6c8f1f91a836965d755bb565f13a80c4a
elmira-orooji <elmiraorooji78@gmail.com>
2026-06-10 16:57:44 +0330

    Login validation

---
+++ frontend/my-rag-app/src/schemas/loginSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters"),
+
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters"),
+
+  rememberMe: z.boolean(),
+});
+
+export type LoginSchemaType = z.infer<typeof loginSchema>;
