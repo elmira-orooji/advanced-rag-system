eb29947f9f65ad1f394c98a5bfcb39ea25964971
elmira-orooji <elmiraorooji78@gmail.com>
2026-06-14 23:53:44 +0330

    redesign-Task 4

--- frontend/my-rag-app/src/pages/DashboardPage.tsx
+++ frontend/my-rag-app/src/pages/DashboardPage.tsx
@@ -316,6 +316,37 @@ const handleSendMessage = (
   </span>
 
 </div>
+<div className="flex items-center gap-4">
+
+  <div className="w-2 h-2 rounded-full bg-blue-500" />
+
+  <span className="flex-1">
+    Collaborate with developers on implementation detail
+  </span>
+
+  <span className="
+    rounded-full
+    bg-slate-100
+    text-slate-600
+    px-3
+    py-1
+    text-sm
+  ">
+    To do
+  </span>
+
+  <span className="
+    rounded-full
+    bg-cyan-100
+    text-cyan-600
+    px-3
+    py-1
+    text-sm
+  ">
+    By tomorrow
+  </span>
+
+</div>
 
       <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
 
