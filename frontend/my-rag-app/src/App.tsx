8c457fb43bf97221c276aa2360f5f426d07671b3
elmira-orooji <elmiraorooji78@gmail.com>
2026-06-17 19:51:01 +0330

    RTL & LTR language setting

--- frontend/my-rag-app/src/App.tsx
+++ frontend/my-rag-app/src/App.tsx
@@ -6,8 +6,21 @@ import {
 
 import LoginPage from "./pages/LoginPage";
 import AppLayout from "./layouts/AppLayout";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
 
 export default function App() {
+  
+    const { i18n } = useTranslation();
+
+  useEffect(() => {
+    document.documentElement.dir =
+      i18n.language === "fa"
+        ? "rtl"
+        : "ltr";
+  }, [i18n.language]);
+
   return (
     <BrowserRouter>
       <Routes>
