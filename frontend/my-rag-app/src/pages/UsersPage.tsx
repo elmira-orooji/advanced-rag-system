0d23d495fb52bab7942e7a3114633de685340be8
elmira-orooji <elmiraorooji78@gmail.com>
2026-06-19 21:55:03 +0330

    َImport icons to Users page

--- frontend/my-rag-app/src/pages/UsersPage.tsx
+++ frontend/my-rag-app/src/pages/UsersPage.tsx
@@ -1,9 +1,8 @@
-export default function UsersPage() {
-  return (
-    <div className="h-full p-10">
-      <h1 className="text-3xl font-bold">
-        Users Management
-      </h1>
-    </div>
-  );
-}
+import {
+  UserPlus,
+  Pencil,
+  Trash2,
+} from "lucide-react";
+
+
+
