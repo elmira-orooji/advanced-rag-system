c79082112f069580201ed913fbbfc5bbffc98180
elmira-orooji <elmiraorooji78@gmail.com>
2026-06-19 21:57:57 +0330

    َUsers page General struction

--- frontend/my-rag-app/src/pages/UsersPage.tsx
+++ frontend/my-rag-app/src/pages/UsersPage.tsx
@@ -5,4 +5,9 @@ import {
 } from "lucide-react";
 
 
-
+<motion.div
+  initial={{ opacity: 0, y: 10 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 0.25 }}
+  className="grid grid-cols-12 h-full"
+>
