7a09896a6502298a6778b238c340ac890b531a8d
elmira-orooji <elmiraorooji78@gmail.com>
2026-08-28 00:16:37 +0330

    style: unify dropdowns and selection menus

--- frontend/my-rag-app/src/main.tsx
+++ frontend/my-rag-app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import AppToaster from "./components/AppToaster";
 import "./i18n";
 import "./index.css";
+import "./styles/dropdowns.css";
 import App from "./App";
 
 createRoot(
